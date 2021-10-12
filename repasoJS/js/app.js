322fb4930c72b46dc76ef7485345645d7f7eb102
Sofia Guajardo <sofiarg94@gmail.com>
2021-10-12 11:35:33 -0300

    Repaso Javascript

---
+++ repasoJS/js/app.js
@@ -0,0 +1,63 @@
+// alert('estamos en el archivo externo')
+// const nombres=["Luis", "Tomas", "Lilian", "Cecilia"]
+// nombres.push("Regulo")
+// console.log(nombres)
+// const manejoNombres=(a)=>{
+//     nombres.push(a)
+//     console.log(nombres)
+// };
+
+// objetos
+// const agregarNombres=()=>{
+//     let nombre={
+//         nombre:"Luis",
+//         apellido:"Navas"
+//     };
+//     nombres.push(nombre)
+//     console.log(nombres)
+// }
+// const nomb=()=>{
+//     let nombres ={
+//         nombre: "Regulo",
+//         apellido: "Luna"
+//     }
+//     console.log(nombre);
+// }
+// nomb()
+// agregarNombre()
+
+class Persona{
+    constructor(nombre,apellido){
+        this.nombre=nombre;
+        this.apellido=apellido;
+    }
+    mostrarNombre(){
+        return this.nombre;
+
+    }
+}
+let nombre = prompt("Escribe tu nombre")
+let persona1 = new Persona(nombre, "Navas")
+let persona2 = new Persona("Regulo", "Luna")
+let persona3={
+    nombre:"Pedro",
+    apellido:"Mansilla"
+}
+console.log(persona1.mostrarNombre())
+console.log(persona2.mostrarNombre())
+
+class Vehiculo{
+    encender(){
+        return "el auto está encendido"
+    }
+}
+let auto=new Vehiculo();
+auto.velocidad=150;
+let moto={
+    encender:()=>{
+        return "Está encendida la moto"
+    }
+}
+console.log(auto.encender())
+console.log(auto.velocidad)
+console.log(moto.encender())
