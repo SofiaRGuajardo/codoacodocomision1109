8ad1bc556706bc90f868167d9eff8edaabcbf5ab
Sofia Guajardo <sofiarg94@gmail.com>
2021-11-06 12:33:31 -0300

    clase 2 Java

---
+++ NetBeans/Clase2Java/src/clase2java/Clase2Java.java
@@ -0,0 +1,25 @@
+
+package clase2java;
+
+import java.util.Scanner;
+
+public class Clase2Java {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        System.out.println("Ingrese su nombre de usuario: ");
+        String nombre = entrada.nextLine();
+        System.out.println("Ingrese su contraseña: ");
+        String pass = entrada.nextLine();
+        Ingreso ing = new Ingreso();
+        ing.setUsuario(nombre);
+        ing.setPass(pass);
+        System.out.println(ing.Validar());
+        /*
+        if(!"Sofia".equals(nombre)) {
+            System.out.println("El usuario es incorrecto");
+        } else {
+            System.out.println("Bienvenida " + nombre);
+        }*/
+    } 
+}
